c29aa326ba2f2430da61f0aa4a846139b48f0639
ngieminv <ngieminv@LB228-17.erau.edu>
2016-11-19 19:22:45 -0500

    Master now can plot

--- Telemtry/src/Masterstation/MasterGUI.java
+++ Telemtry/src/Masterstation/MasterGUI.java
@@ -1,10 +1,20 @@
 package Masterstation;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import javax.swing.SwingUtilities;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import javax.swing.SwingUtilities;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.embed.swing.SwingNode;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -72,7 +82,13 @@ public class MasterGUI extends Application {
 	private Tab Plot;
 	//plotGridPane
 	private GridPane plotGridPane;
-
+	
+	//for plot
+	static int Xcount=1;
+	static int Ycount=1; // counting number of line print
+	static int counter=100;
+	static String file1="E:\\java\\matlab test\\test1.txt";
+	static String file2="E:\\java\\matlab test\\test2.txt";
 	
 	private Axis<Number> xAxis1, xAxis2, xAxis3, xAxis4;
 	private Axis<Number> yAxis1, yAxis2, yAxis3, yAxis4;
@@ -412,18 +428,88 @@ public class MasterGUI extends Application {
 		tabPanePlots.getTabs().addAll(Plot);
 		Plot.setClosable(true);
 		borderPane.setCenter(tabPanePlots);
+		//Add a swing node
+        final SwingNode swingNode = new SwingNode();        
+        createSwingContent(swingNode);
 		 //Plots in tabPane Plot1
-  		plotGridPane = new GridPane();
-  		plotGridPane.setHgap(10);
-		plotGridPane.setVgap(10);
-  		Plot.setContent(plotGridPane);
-  		plotGridPane.add(lineChart1, 1, 1);
-  		plotGridPane.add(lineChart2, 1, 2);
-  		plotGridPane.add(lineChart3, 2, 1);
-  		plotGridPane.add(lineChart4, 2, 2);
-  		
-  		//lineChart.setVisible(true);
+        plotGridPane = new GridPane();        		
+		plotGridPane.setHgap(10);
+        plotGridPane.setVgap(10);
+        plotGridPane.getChildren().add(swingNode);  
+		Plot.setContent(plotGridPane);	
+//		plotGridPane.add(lineChart1, 1, 1);
+//		plotGridPane.add(lineChart2, 1, 2);
+//		plotGridPane.add(lineChart3, 2, 1);
+//  		plotGridPane.add(lineChart4, 2, 2); 	
+  
+//lineChart.setVisible(true);
 	}
+	// Creating Swing node for plot
+	private void createSwingContent(SwingNode swingNode) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            	XYSeries series = new XYSeries("Light Sensor Readings");
+            	XYSeriesCollection dataset = new XYSeriesCollection(series);
+        		JFreeChart chart = ChartFactory.createScatterPlot("Light Sensor Readings", "Time (seconds)", "ADC Reading", dataset);
+        		ChartPanel plot= new ChartPanel(chart);        		
+        		swingNode.setContent(plot);
+        		// create a new thread that listens for incoming text and populates the graph
+        		Thread thread = new Thread(){     			
+        			
+        			@Override public void run() {
+        				while(Ycount<counter){//loop
+        				
+        		
+        				File temp = new File(file1);
+        				File temp2= new File(file2);
+
+        				try (BufferedReader br = new BufferedReader(new FileReader(temp))) {	
+        				     
+        					try(BufferedReader br2 = new BufferedReader(new FileReader(temp2))){
+        					    String line ;
+        					    String line2;
+        					    int count=0;
+        					 while ((line = br.readLine()) != null && (line2 = br2.readLine()) != null) {		
+        			        	count++;//count number of line 
+        			        	System.out.printf("Fist count: %d \n",count);
+        			        	System.out.printf("Secount Xcount: %d \n",Xcount);
+        			        	System.out.printf("Secount Ycount: %d \n",Ycount);
+        			        	// if the number of new line is added then do it 
+        			            if( Xcount == count ) {	     			    
+        							        
+        						            if( Ycount == count ) {
+        						            double number = Double.parseDouble(line);
+        									System.out.println(number);				
+        									double number2 = Double.parseDouble(line2);
+        						            System.out.println(number2);	
+        									series.add(number, number2);
+        									plot.repaint();
+        									Ycount++;
+        									Xcount++;
+        									counter++;
+        					                }
+        			            }    
+        			        }
+        					}catch(IOException e){
+        						e.printStackTrace();
+        					}
+        							}catch(IOException e){
+        								e.printStackTrace();
+        							}					
+        			            }
+        			        }					
+
+        			
+        		
+        			//end while
+        		};
+        		thread.start();
+            }
+        });
+		
+	}
+
 	/**
 	}
 	 * Displays DataSincFreq window
